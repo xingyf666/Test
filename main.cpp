36dc8414abd74145e71d57309aa23fc36eabfb1b
xingyf666 <814083398@qq.com>
2024-05-28 11:25:52 +0800

    rebase

--- main.cpp
+++ main.cpp
@@ -5,5 +5,14 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
+<<<<<<< HEAD
     return 0;
 }
+=======
+    cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl;
+    return 0;
+}
+>>>>>>> e265cb2 (merge)
